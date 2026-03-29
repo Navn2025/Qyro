9e508354f2e9c0718b196583cba931d4146312d6
Navn2025 <navneett546@gmail.com>
2026-03-30 04:49:45 +0530

    fixed getpass error

--- db/db.py
+++ db/db.py
@@ -9,8 +9,5 @@ from models.models import Base
 DATABASE_URI = os.getenv("DATABASE_URI", "postgresql://postgres:1234@localhost:5432/q&a_dataset")
 engine = create_engine(DATABASE_URI)
 
-# Ensure local tables exist in the primary DB (Settings, History etc.)
-Base.metadata.create_all(bind=engine)
-
 # The Session maker for the primary database
 Session = sessionmaker(bind=engine)
